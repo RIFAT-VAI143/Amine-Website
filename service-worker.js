3e95832fcdc03a74a82d6ca3bad00808c04ebb5b
RIFAT-VAI143 <144621265+RIFAT-VAI143@users.noreply.github.com>
2026-04-19 11:33:17 +0600

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-// ================= FILE: service-worker.js (optional PWA) =================
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-self.addEventListener('fetch', event => {
-  // simple passthrough for offline capability
-});
